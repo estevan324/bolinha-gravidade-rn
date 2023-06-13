3a18e2be511a1989776f38497d232be935e84388
Estevan Hernandes Silva de Oliveirs <estevanhernandes1@gmail.com>
2023-06-13 17:43:35 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Ball } from './components/Ball';
+import { styles } from './styles';
+
+let timer: number;
+
+const App = () => {
+  const [gravity, setGravity] = useState(0.98);
+  const [upForce, setUpForce] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [posY, setPosY] = useState(0);
+
+  useEffect(() => {
+    const applyGravity = () => {
+      // Decreasing upForce
+      let newUpForce = upForce - gravity;
+      newUpForce = newUpForce < 0 ? 0 : newUpForce
+      setUpForce(newUpForce)
+
+      // Modifying speed
+      let newSpeed = speed + (gravity - (newUpForce / 2))
+      setSpeed(newSpeed)
+
+      // Setting new position based on speed
+      let newPosY = posY - newSpeed
+
+      if (newPosY < 0) {
+        newPosY = 0
+        setSpeed(0)
+      }
+
+      setPosY(newPosY)
+    };
+
+    clearTimeout(timer);
+    timer = setTimeout(applyGravity, 30);
+  }, [gravity, upForce, speed, posY]);
+
+  const handleForceButton = () => {
+    setUpForce(7)
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.area}>
+        <Ball posY={posY} />
+      </View>
+      <View style={styles.control}>
+        <View>
+          <Text style={styles.controlText}>UpForce: {upForce.toFixed(2)}</Text>
+          <Text style={styles.controlText}>Speed: {speed.toFixed(2)}</Text>
+          <Text style={styles.controlText}>PosY: {posY.toFixed(2)}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.controlButton}
+          onPress={handleForceButton}>
+          <Text style={styles.controlButtonText}>Fazer Força</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default App;
---
+++ styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+  },
+  area: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+  },
+  control: {
+    flexDirection: 'row',
+    gap: 20,
+    alignItems: 'center',
+    margin: 20,
+  },
+  controlText: {
+    color: '#fff',
+  },
+  controlButton: {
+    flex: 1,
+    height: 100,
+    backgroundColor: '#0061FF',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  controlButtonText: {
+    color: '#FFF'
+  }
+});
